56e4147f8047a44dfdfb3c3a04169a0580d4352c
wanedri <50620343+wanedri@users.noreply.github.com>
2026-09-07 11:11:22 +0800

    terraform (#1)
    * WIP: add basic terraform file
    
    * WIP:Add gitignore and make some adjustment
    
    * WIP:Add name/tag for the subnet, route table,gateway
    
    ---------
    
    Co-authored-by: wanedri <wanadri@mirfalah.my>

---
+++ terraform/ec2.tf
@@ -0,0 +1,63 @@
+data "aws_ami" "my_ami" {
+    most_recent = true
+    owners      = ["099720109477"]
+
+    filter {
+        name   = "name"
+        values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+    }
+}
+
+data "aws_iam_instance_profile" "my_ssm_profile" {
+  name = "EC2-SSM-Role"
+}
+
+data "aws_ssm_parameter" "token" {
+  name = "/devops-bootcamp-2026/tunnel-token"
+}
+
+module "web_server" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 6.0"
+  name                   = "web-server"
+  ami                    = data.aws_ami.my_ami.id
+  instance_type          = "t3.micro"
+  subnet_id              = module.my_vpc.public_subnets[0]
+  create_security_group  = false
+  vpc_security_group_ids = [aws_security_group.public.id]
+  key_name               = "wan-adri-key"
+  create_eip = true
+  private_ip = "10.0.0.5"
+  iam_instance_profile = data.aws_iam_instance_profile.my_ssm_profile.name
+  tags                   = { Name = "web-server" }
+}
+
+module "ansible_server" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 6.0"
+  name                   = "ansible-server"
+  ami                    = data.aws_ami.my_ami.id
+  instance_type          = "t3.micro"
+  subnet_id              = module.my_vpc.private_subnets[0]
+  create_security_group  = false
+  vpc_security_group_ids = [aws_security_group.private.id]
+  key_name               = "wan-adri-key"
+  private_ip = "10.0.0.135"
+  iam_instance_profile = data.aws_iam_instance_profile.my_ssm_profile.name
+  tags                   = { Name = "ansible-server" }
+}
+
+module "monitoring_server" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 6.0"
+  name                   = "monitoring-server"
+  ami                    = data.aws_ami.my_ami.id
+  instance_type          = "t3.micro"
+  subnet_id              = module.my_vpc.private_subnets[0]
+  create_security_group  = false
+  vpc_security_group_ids = [aws_security_group.private.id]
+  key_name               = "wan-adri-key"
+  private_ip = "10.0.0.136"
+  iam_instance_profile = data.aws_iam_instance_profile.my_ssm_profile.name
+  tags                   = { Name = "monitoring-server" }
+}
---
+++ terraform/security.tf
@@ -0,0 +1,51 @@
+resource "aws_security_group" "public" {
+   name        = "devops-public-sg"
+   vpc_id      = module.my_vpc.vpc_id
+
+    ingress {
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["10.0.0.0/24"]
+    }
+
+    ingress {
+        from_port = 9100
+        to_port = 9100
+        protocol = "tcp"
+        cidr_blocks = ["10.0.0.136/32"]
+    }
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+resource "aws_security_group" "private" {
+   name        = "devops-private-sg"
+   vpc_id      = module.my_vpc.vpc_id
+
+    ingress {
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["10.0.0.0/24"]
+    }
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
